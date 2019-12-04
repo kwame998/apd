6d792be8534f8f7f90e2a2eb62ebf7619f1b7759
steven <guozhaolong@gmail.com>
2019-12-04 09:27:28 +0800

    attr default change to isDefault

--- src/components/Detail/PushButtonDetail.js
+++ src/components/Detail/PushButtonDetail.js
@@ -15,7 +15,7 @@ const PushButtonDetail = ({ widget, getFieldDecorator }) => {
       <Form.Item label="事件">{getFieldDecorator('event',{ initialValue: detail.event })(<Input />)}</Form.Item>
       <Form.Item label="目标标识">{getFieldDecorator('targetId',{ initialValue: detail.targetId })(<Input />)}</Form.Item>
       <Form.Item label="值">{getFieldDecorator('value',{ initialValue: detail.value })(<Input />)}</Form.Item>
-      <Form.Item label="缺省按钮">{getFieldDecorator('default',{ initialValue: detail.default })(<Checkbox />)}</Form.Item>
+      <Form.Item label="缺省按钮">{getFieldDecorator('isDefault',{ initialValue: detail.isDefault })(<Checkbox />)}</Form.Item>
     </>
   )
 };
