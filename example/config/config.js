01d1b24c2b7151aa14855cd4bf49d1fa0f707af0
steven <guozhaolong@gmail.com>
2020-01-09 16:03:47 +0800

    删除dva

--- example/config/config.js
+++ example/config/config.js
@@ -6,9 +6,6 @@ export default {
     [
       'umi-plugin-react',
       {
-        dva: {
-          immer: true
-        }
       },
     ]
   ],
--- example/services/graphql.js
+++
@@ -1,8 +0,0 @@
-import request from '../utils/request';
-
-export async function query(params,variables) {
-  return request(`/api/graphql`, {
-    method: 'POST',
-    data: {query:params,variables},
-  });
-}
